b229362ab317364754baf439c5a7452dc7582624
ducchy <nozu_soichiro@grenge.co.jp>
2023-04-21 00:55:50 +0900

    ロードの並列化

--- Assets/Scripts/dtank/SceneSituation/Battle/BattleDataUtility.cs
+++ Assets/Scripts/dtank/SceneSituation/Battle/BattleDataUtility.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using System.Linq;
 using Cysharp.Threading.Tasks;
 using GameFramework.Core;
 
@@ -11,19 +11,28 @@ namespace dtank
         public static async UniTask<BattleModelSetupData> CreateBattleModelSetupDataAsync(
             BattleEntryData entryData, IScope scope)
         {
-            var tankModelSetupDataDict = new Dictionary<int, BattleTankModelSetupData>();
-            foreach (var player in entryData.Players)
-                tankModelSetupDataDict.Add(player.PlayerId, await CreateTankModelSetupDataAsync(player, scope));
-
-            var ruleData = await new BattleRuleDataAssetRequest(entryData.RuleId)
+            var createTankModelSetupDataTasks =
+                UniTask.WhenAll(
+                    entryData.Players
+                        .Select(player => CreateTankModelSetupDataAsync(player, scope))
+                        .ToArray());
+            var loadRuleDataTask = new BattleRuleDataAssetRequest(entryData.RuleId)
                 .LoadAsync(scope)
                 .ToUniTask();
+            
+            var (tankModelSetupDataDictPairs, ruleData) =
+                await UniTask.WhenAll(createTankModelSetupDataTasks, loadRuleDataTask);
+
+            var tankModelSetupDataDict = tankModelSetupDataDictPairs
+                .ToDictionary(
+                    tankModelSetupDataDictPair => tankModelSetupDataDictPair.Item1,
+                    tankModelSetupDataDictPair => tankModelSetupDataDictPair.Item2);
 
             return new BattleModelSetupData(ruleData, tankModelSetupDataDict);
         }
 
         /// <summary>BattleTankModelのSetupに必要なデータの作成</summary>
-        private static async UniTask<BattleTankModelSetupData> CreateTankModelSetupDataAsync(
+        private static async UniTask<(int, BattleTankModelSetupData)> CreateTankModelSetupDataAsync(
             BattlePlayerEntryData player, IScope scope)
         {
             var parameterData = await new BattleTankParameterDataAssetRequest(player.ParameterId)
@@ -34,7 +43,7 @@ namespace dtank
                 .LoadAsync(scope)
                 .ToUniTask();
 
-            return new BattleTankModelSetupData(parameterData, actorSetupData);
+            return (player.PlayerId, new BattleTankModelSetupData(parameterData, actorSetupData));
         }
     }
 }
