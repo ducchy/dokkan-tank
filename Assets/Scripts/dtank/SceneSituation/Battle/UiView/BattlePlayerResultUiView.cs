75379f1fe87b3ecde8f0df503b01aa78ea326fbd
ducchy <nozu_soichiro@grenge.co.jp>
2023-04-23 20:47:12 +0900

    BattlePlayerResultuiViewのアニメーション実装リファクタ

--- Assets/Scripts/dtank/SceneSituation/Battle/UiView/BattlePlayerResultUiView.cs
+++ Assets/Scripts/dtank/SceneSituation/Battle/UiView/BattlePlayerResultUiView.cs
@@ -1,16 +1,13 @@
 using System;
 using BrunoMikoski.AnimationSequencer;
-using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using Sequence = DG.Tweening.Sequence;
 
 namespace dtank
 {
     public class BattlePlayerResultUiView : MonoBehaviour, IDisposable
     {
-        [SerializeField] private CanvasGroup _group;
         [SerializeField] private TextMeshProUGUI _playerName;
         [SerializeField] private TextMeshProUGUI _rank;
         [SerializeField] private Image _rankBg;
@@ -18,15 +15,12 @@ namespace dtank
         [SerializeField] private AnimationSequencerController _arrowSeq;
         [SerializeField] private AnimationSequencerController _openSeq;
 
-        private Sequence _sequence;
-
         public void Setup(string playerName, Color color, int rank, bool isMine)
         {
             _playerName.text = playerName;
             _rankBg.color = color;
             _rank.text = rank.ToString();
             _arrow.gameObject.SetActive(isMine);
-            color.a = 0f;
             _arrow.color = color;
             
             _openSeq.Play(PlayArrow);
@@ -35,7 +29,6 @@ namespace dtank
 
         public void Dispose()
         {
-            _sequence?.Kill();
             _openSeq.Kill();
             _arrowSeq.Kill();
         }
@@ -51,16 +44,6 @@ namespace dtank
         public void Open()
         {
             _openSeq.Resume();
-        }
-
-        public void Close()
-        {
-            _sequence?.Kill();
-
-            _sequence = DOTween.Sequence()
-                .Append(_group.DOFade(0f, 0.3f))
-                .SetLink(gameObject)
-                .Play();
         }
     }
 }
